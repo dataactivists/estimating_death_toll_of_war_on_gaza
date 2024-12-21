feee9d324a7abdbc1a3190bb16f2693bb9943ea0
dataactivists <contact@dataactivists.org>
2024-12-21 19:52:23 +0100

    refactor: add config for scripts

---
+++ code/config.py
@@ -0,0 +1,8 @@
+# code/config.py
+
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+CODE_DIR = ROOT_DIR / 'code'
+DATA_DIR = ROOT_DIR / 'data'
+CHARTS_DIR = ROOT_DIR / 'charts'
